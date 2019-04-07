48be852e84deac3e8a3d921f7afe123128418573
Michał Olekszy <michal.olekszy@gmail.com>
2019-04-07 20:37:23 +0200

    Create Model Product_Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :product_categories
   resources :ingredients
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
