113545e82d79ab0359cfd4e2a2f3f5605f31239a
Michał Olekszy <michal.olekszy@gmail.com>
2019-04-12 18:43:10 +0200

    Changes in Models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  has_many :ingredients, dependent: :destroy
+  has_many :allergens
+  has_one :brand
+  has_one :product_category
 end
--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,2 +1,3 @@
 class ProductCategory < ApplicationRecord
+  belongs_to :product
 end
