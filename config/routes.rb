25cf347802a8fcde1aeed4fa70c1e043795ad57c
Michał Olekszy <michal.olekszy@gmail.com>
2019-05-09 18:23:12 +0200

    Update devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login' }
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
+  root to: "welcome#index"
+
   resources :products
   resources :allergens
   resources :brands
   resources :product_categories
   resources :ingredients
 
-  root to: "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
