551d8f2b6b51fb6b0a46d13fb3fb49241d546b7e
Michał Olekszy <michal.olekszy@gmail.com>
2019-05-12 09:44:15 +0200

    Changes in: Logo and Avatar, allergen dropdown and product link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :authenticate_admin
    before_action :configure_permitted_parameters, if: :devise_controller?
+   before_action :set_allergens
 
    def authenticate_admin
      return if current_user.admin?
@@ -12,5 +13,9 @@ class ApplicationController < ActionController::Base
 
    def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:account_update, keys: [:pesel, :birth_date, :avatar ])
+   end
+
+   def set_allergens
+     @allergens ||= Allergen.all
    end
 end
