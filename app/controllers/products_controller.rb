59444277231292d3087aeab9c7bb410c75239ca2
Michał Olekszy <michal.olekszy@gmail.com>
2019-06-06 20:09:13 +0200

    First Part of adding favorites -correction 2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy, :favorite]
 
   # GET /products
   # GET /products.json
@@ -61,6 +61,21 @@ class ProductsController < ApplicationController
     end
   end
 
+  def favorite
+     type = params[:type]
+     if type == "favorite"
+       current_user.favorites << @product
+       redirect_back(fallback_location: product_path)
+
+     elsif type == "unfavorite"
+       current_user.favorites.delete(@product)
+       redirect_back(fallback_location: product_path)
+
+     else
+       # Type missing, nothing happens
+       redirect_back(fallback_location: product_path)
+     end
+   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -72,21 +87,7 @@ class ProductsController < ApplicationController
       @myAmount = Amount.find_by(product: product, ingredient: ingredient)
     end
 
-    def favorite
-       type = params[:type]
-       if type == "favorite"
-         current_user.favorites << @product
-         redirect_to :back, notice: 'You favorited #{@product.name}'
 
-       elsif type == "unfavorite"
-         current_user.favorites.delete(@product)
-         redirect_to :back, notice: 'Unfavorited #{@product.name}'
-
-       else
-         # Type missing, nothing happens
-         redirect_to :back, notice: 'Nothing happened.'
-       end
-     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :avatar, :product_category_id, :brand_id, :description, {ingredient_ids: []}, {allergen_ids: []}, :my_amount)
